c3f6936bb6c02871d513d85252178a6f92dc5d17
walbrunt <79297850+walbrunt@users.noreply.github.com>
2024-03-29 12:37:02 -0500

    Add files via upload

---
+++ Python and Stats for Fin Analysis_Population+and+Sample.py
@@ -0,0 +1,86 @@
+
+# coding: utf-8
+
+# ## Population and Sample
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[17]:
+
+
+# Create a Population DataFrame with 10 data 
+
+data = pd.DataFrame()
+data['Population'] = [47, 48, 85, 20, 19, 13, 72, 16, 50, 60]
+
+
+# You may get different results from sampling.
+
+# In[13]:
+
+
+# Draw sample with replacement, size=5 from Population
+
+a_sample_with_replacement = data['Population'].sample(5, replace=True)
+print(a_sample_with_replacement)
+
+
+# In[15]:
+
+
+# Draw sample without replacement, size=5 from Population
+
+a_sample_without_replacement = data['Population'].sample(5, replace=False)
+print(a_sample_without_replacement)
+
+
+# # Parameters and Statistics
+
+# In[18]:
+
+
+# Calculate mean and variance
+population_mean = data['Population'].mean()
+population_var = data['Population'].var()
+print('Population mean is ', population_mean)
+print('Population variance is', population_var)
+
+
+# **Expected Output: ** Population mean is  43.0
+# Population variance is 571.8
+# 
+
+# You may get different result from sampling.
+
+# In[19]:
+
+
+# Calculate sample mean and sample standard deviation, size =10
+# You will get different mean and varince every time when you excecute the below code
+
+a_sample = data['Population'].sample(10, replace=True)
+sample_mean = a_sample.mean()
+sample_var = a_sample.var()
+print('Sample mean is ', sample_mean)
+print('Sample variance is', sample_var)
+
+
+# # Average of an unbiased estimator
+
+# In[20]:
+
+
+sample_length = 500
+sample_variance_collection=[data['Population'].sample(10, replace=True).var(ddof=1) for i in range(sample_length)]
+
+
+# In[ ]:
+
+
+
+
